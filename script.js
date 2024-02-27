3a24d1bd83b3108094746f40c32bda99face6f89
Daniela Marques Correia <danedcmc@gmail.com>
2024-02-27 15:00:28 -0300

    user input added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const randomdisplay = document.getElementById("random-number");
 const randomizebtn = document.getElementById("randomize-btn");
 const value = document.getElementById("value");
 const send = document.getElementById("send");
+const displaynumber = document.getElementById("used-number"");
 
 //let howmany = prompt("How many number?")
 
@@ -10,4 +11,15 @@ let howmany = 10;
 randomizebtn.onclick = function () {
     let randomnumber = Math.floor(Math.random()*howmany + 1);
     randomdisplay.innerText = randomnumber;
+}
+
+send.onclick = function () {
+    if (value <= 2) { 
+        howmany = 10;
+        displaynumber.innerText = howmany; 
+    }
+    else {
+        howmany = value.Number()
+        displaynumber.innerText = howmany; 
+    }
 }
